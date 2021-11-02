0c0fedf011015e99b48d424553527d789079e525
daRoyalCacti <srdrmrdave@gmail.com>
2021-11-03 09:28:35 +1000

    Cleaned up the window struct

--- window.hpp
+++ window.hpp
@@ -8,14 +8,14 @@
 #include <GLFW/glfw3.h>
 
 struct Window {
-    GLFWwindow* window; //object to hold the window and a context
+    GLFWwindow* window{}; //object to hold the window and a context
     const int window_width = 800;   //values are ints because thats what GLFW expects them as
     const int window_height = 600;
     const char* window_name = "Vulkan";
 
     void makeWindow();
     void cleanup();
-    inline GLFWwindow* const& get_window() const {
+    [[nodiscard]] inline GLFWwindow* const& get_window() const {
         return window;
     }
 };
